77db30f8e2ccb9ac7c0b4637a7b8258fd75610a1
Joe Han <jhoon.han@outlook.com>
2024-07-27 09:06:46 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,34 +1,119 @@
 import "./scss/App.scss";
 import React, { useEffect, useContext, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+
+import Roster from "components/Roster";
+import RosterForm from "components/RosterForm";
+
+import { fetchAllUsers, fetchShifts, fetchShiftsByMonthYear } from "./actions";
 
 import useContextValues from "./useContextValues";
 
 export const AppContext = React.createContext();
 
 function App() {
+  // const location = useLocation();
   const contextValues = useContextValues();
+  const { computedShifts, users, selectedUser, selectedShift } = contextValues;
+  const [formMonth, setFormMonth] = useState(8);
+  const [formYear, setFormYear] = useState(2024);
+  const [formUserId, setFormUserId] = useState(1);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    // fetchAllUsers(users);
+  }, []);
+
+  const handleRequestFormSubmit = async (e) => {
     e.preventDefault();
-    console.log(e.target.value);
+    try {
+      await fetchShiftsByMonthYear(
+        computedShifts.state,
+        computedShifts.setData,
+        {
+          month: formMonth,
+          year: formYear,
+        },
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleRequestFormChange = (e) => {
+    const { value } = e.target;
+    e.target.id === "userId" && setFormUserId(value);
+  };
+
+  const handleUserSelect = async (e) => {
+    e.preventDefault();
+    selectedUser.setData(+e.target.value);
+  };
+
+  const renderRequestForm = () => {
+    return (
+      <>
+        <h2>Make a Request</h2>
+        <form onSubmit={handleRequestFormSubmit}>
+          <label htmlFor="userId">User Id : {selectedUser.state}</label>
+          <br />
+          <label htmlFor="shiftId">Shift Id : {selectedShift.state}</label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </>
+    );
+  };
+
+  const renderUserSelectForm = () => {
+    const dummySelector = () => {
+      return Array.from({ length: 14 }, (_, i) => i + 1).map((value) => (
+        <option key={value} value={value}>
+          {value}
+        </option>
+      ));
+    };
+
+    return (
+      <>
+        <h2>Select User</h2>
+        <form>
+          <select
+            id="user"
+            value={selectedUser.state}
+            onChange={handleUserSelect}
+          >
+            {/*{users.state.map((user) => {*/}
+            {/*  return (*/}
+            {/*    <option value={user.user_id} key={user.user_id}>*/}
+            {/*      {user.user_id}*/}
+            {/*    </option>*/}
+            {/*  );*/}
+            {/*})}*/}
+            {dummySelector()}
+          </select>
+        </form>
+      </>
+    );
   };
 
   return (
-    <div className="App">
-      {/*    write me a five option dropdown */}
-      <form onSubmit={handleSubmit}>
-        <select>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-          <option value="3">Option 3</option>
-          <option value="4">Option 4</option>
-          <option value="5">Option 5</option>
-        </select>
+    <AppContext.Provider value={contextValues}>
+      <div className="App">
+        {/*    write me a five option dropdown */}
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+        {renderUserSelectForm()}
+
+        <br />
+        <RosterForm />
+        <Roster />
+        <br />
+        {renderRequestForm()}
+      </div>
+    </AppContext.Provider>
   );
 }
 
--- src/useContextValues.js
+++ src/useContextValues.js
@@ -1,17 +1,46 @@
 import { useState } from "react";
 
- const useContextValues = () => {
-  const [shiftStatus, setShiftStatus] = useState('open');
+const useContextValues = () => {
+  const [shiftStatus, setShiftStatus] = useState("open");
+  const [shifts, setShifts] = useState([]);
+  const [openShifts, setOpenShifts] = useState([]);
+  const [closedShifts, setClosedShifts] = useState([]);
+  const [computedShifts, setComputedShifts] = useState({
+    allRequests: [],
+    closed: [],
+    conflicts: [],
+    open: [],
+  });
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(1);
+  const [selectedShift, setSelectedShift] = useState(1);
+  const [formData, setFormData] = useState({
+    month: 8,
+    year: 2024,
+  });
 
-  const values = {
-      shiftStatus: {
-          state: shiftStatus,
-          set: setShiftStatus,
-        },
-  }
-
-  return values;
-
-}
+  return {
+    computedShifts: {
+      state: computedShifts,
+      setData: setComputedShifts,
+    },
+    users: {
+      state: users,
+      setData: setUsers,
+    },
+    selectedUser: {
+      state: selectedUser,
+      setData: setSelectedUser,
+    },
+    selectedShift: {
+      state: selectedShift,
+      setData: setSelectedShift,
+    },
+    formData: {
+      state: formData,
+      setData: setFormData,
+    },
+  };
+};
 
 export default useContextValues;
